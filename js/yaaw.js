fc816ad4146efbedc058c64132a4c56150b4389b
cc <cc@TPX1CARCH>
2017-11-01 10:47:18 +0100

    Change finish_notification = 0 by default

--- js/yaaw.js
+++ js/yaaw.js
@@ -817,7 +817,7 @@ var YAAW = (function() {
       init: function() {
         this.jsonrpc_path = $.Storage.get("jsonrpc_path") || location.protocol+"//"+(location.host.split(":")[0]||"localhost")+":6800"+"/jsonrpc";
         this.refresh_interval = Number($.Storage.get("refresh_interval") || 10000);
-        this.finish_notification = Number($.Storage.get("finish_notification") || 1);
+        this.finish_notification = Number($.Storage.get("finish_notification") || 0);
         this.add_task_option = $.Storage.get("add_task_option");
         this.jsonrpc_history = JSON.parse($.Storage.get("jsonrpc_history") || "[]");
         if (this.add_task_option) {
